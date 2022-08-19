2cef0af082fdf0d336a752badf8ee3d9aa94a4ff
Rafaela de Paula Mendes <rafaeladepaulamendes@gmail.com>
2022-08-18 23:37:24 -0300

    CadastroDeEventos

---
+++ evento.js
@@ -0,0 +1,21 @@
+const today = new Date ( 2022 , 8 , 18 ) ;
+dataEvento = nova data ( "2022-09-21" )
+console . log ( "----------" )
+
+console . log ( "Insira ano/mês/dia do evento" )
+if ( dataEvento > today ) { console . log ( "Cadastro permitido" ) }
+else { console . log ( "Dados Inválidos" ) }
+console . log ( "----------" )
+
+let idade = 18
+console . log ( "Insira a sua idade." )
+if ( idade >= 18 ) { console . log ( "Cadastro permitido." ) }
+else { console . log ( "Cadastro não permitido para menores de idade." ) }
+console . log ( "----------" )
+
+let participantesEvento = 100
+console . log ( "Verificando número de participantes no evento." )
+if ( participantesEvento >= 100 ) { console . log ( "Cadastro não permitido. Limite de pessoas excedido" ) }
+else { console . log ( "Cadastro realizado com sucesso" ) }
+console . log ( "----------" )
+
